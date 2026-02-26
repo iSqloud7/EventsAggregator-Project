e3f3c14d5c459678f2b66f958da5af9ec9f67dfc
unknown <ivanpupinoski03@gmail.com>
2026-02-26 23:56:50 +0100

    Successfully completed database popularization with previously scraped data.

--- backend/db/models.py
+++ backend/db/models.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-class Event(Base): # TODO
-    tablename = "events"
+class Event(Base):
+    __tablename__ = "events"
     id       = Column(Integer, primary_key=True, autoincrement=True)
-    name     = Column(String, nullable=False)          # ТЕАТАР ЗА ДЕЦА И МЛАДИНЦИ
-    date_from = Column(String, nullable=True)          # 01-02-2026
-    date_to   = Column(String, nullable=True)          # 28-02-2026
-    location  = Column(String, nullable=True)          # ако го нема на таа страна -> None
-    price     = Column(String, nullable=True)          # ако го нема -> None
-    image_url = Column(String, nullable=True)          # URL на постерот
-    link      = Column(String, nullable=True)          # https://mktickets.mk/en/event-en/tdm/
-    source    = Column(String, nullable=True)          # од која страна е скрејпано (mktickets.mk, ...)
+    title     = Column(String, nullable=False)
+    image = Column(String, nullable=True)
+    location   = Column(String, nullable=True)
+    price  = Column(String, nullable=True)
+    time_start     = Column(String, nullable=True)
+    date_start = Column(String, nullable=True)
+    city      = Column(String, nullable=True)
+    description    = Column(String, nullable=True)
---
+++ backend/scripts/load_events.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+from backend.services.event_service import load_events_from_json
+
+file_path = Path("mktickets-scraper/events-output/events.json")
+
+with open(file_path, encoding="utf-8") as f:
+    events = json.load(f)
+
+load_events_from_json(events)
+
+print(f"Loaded {len(events)} events")
